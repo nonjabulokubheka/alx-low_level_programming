3ef654623f214c4757e730034e939b35f9da7e1b
nonjabulokubheka <nonjabulo.kubheka9@gmail.com>
2022-12-08 18:27:05 -0800

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <studio>
+
+/** 
+ * main - Entry point
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
